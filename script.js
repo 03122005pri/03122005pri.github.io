fdc4530ed6d760303828dcea3dc8fe1890d54406
03122005pri <92933548+03122005pri@users.noreply.github.com>
2021-10-24 18:43:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,34 +7,30 @@ if (animItems.length > 0) {
             const animItem = animItems[index];
             const animItemHeight = animItem.offsetHeight;
             const animItemOffset = offset(animItem).top;
-            const animStart = 4;
+            const animStart = 50;
 
             let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
             if (animItemHeight > window.innerHeight) {
                 animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            } 
+            };
 
             if ((pageYOffset >  animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
                 animItem.classList.add('_active');
             } else {
                 if (!animItem.classList.contains('_anim-no-hide')) {
                     animItem.classList.remove('_active')
-                }
-            }
-        }
-    }
+                };
+            };
+        };
+    };
+
     function offset(el) {
         const rect = el.getBoundingClientRect(),
             scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
             scrollTop = window.pageYOffset || document.documentElement.scrollTop;
         return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-    }
+    };
 
-    setTimeout(() => { animOnScroll(); }, 300);
-}
-
-function ClickFunction() {
-    document.getElementById("true").innerHTML = "Міф";
-    document.getElementById("p").innerHTML = "Масова вакцинація не тільки не призведе до мутації коронавірусу SARS-CoV-2, а й навпаки допоможе запобігти цьому через колективний імунітет. Якщо щепити значну частину населення, то вірусу не буде, де мутувати, і його нові штами з'являтимуться  з меншою ймовірністю.";
-}
+    setTimeout(() => { animOnScroll(); }, 100);
+};
